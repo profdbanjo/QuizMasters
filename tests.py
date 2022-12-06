083135d5515decd4dbb582ca09c2350455bacf9a
davinderghotra99 <112905335+davinderghotra99@users.noreply.github.com>
2022-12-05 19:26:42 -0800

    Add files via upload
    Testing for all codes

---
+++ tests.py
@@ -0,0 +1,268 @@
+# This is a sample Python script.
+from _pytest.pytester import _config_for_test
+
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pytest
+
+
+def pytest_generate_tests(metafunc):
+    idlist = []
+    argvalues = []
+    for scenario in metafunc.cls.scenarios:
+        idlist.append(scenario[0])
+        items = scenario[1].items()
+        argnames = [x[0] for x in items]
+        argvalues.append([x[1] for x in items])
+    metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
+
+
+def test_db_initialized(db):
+    # a dummy test
+    if db.__class__.__name__ == "DB2":
+        pytest.fail("failed to login")
+
+
+def pytest_generate_tests(metafunc):
+    if "db" in metafunc.fixturenames:
+        metafunc.parametrize("db", ["d1", "d2"], indirect=True)
+
+
+class DB1:
+    "one database object"
+
+
+class DB2:
+    "alternative database object"
+
+
+@pytest.fixture()
+def db(request):
+    if request.param == "d1":
+        return DB1()
+    elif request.param == "d2":
+        return DB2()
+    else:
+        raise ValueError("invalid internal test config")
+
+
+xfail = pytest.mark.xfail
+
+
+@pytest.mark.xfail
+def squdksj():
+    pass
+
+
+def test_login_failing(sgudksj9username=_config_for_test):
+    assert sgudksj9username
+
+
+def test_signup_failing(sgudksj9username=_config_for_test):
+    assert sgudksj9username
+
+
+def test_signup_passing(userinput=_config_for_test, username=_config_for_test, password=_config_for_test):
+    if userinput == username:
+        userinput = input("Password?\n")
+    if userinput == password:
+        print("Welcome!")
+    else:
+        print("That is the wrong password.")
+    print("That is the wrong username.")
+
+
+import pytest
+
+
+class registration:
+    def __init__(self, name):
+        self.name = name
+        self.login = False
+
+    def login(self):
+        self.login = True
+
+
+def email():
+    pass
+
+
+class user:
+    def __init__(self, *user):
+        self.user = email
+        self._user_email()
+
+    def _user_email(self):
+        for user in self.user:
+            user.mail()
+
+    def _user_email(self):
+        pass
+
+
+# Arrange
+@pytest.fixture
+def user_email():
+    return 'students email'
+
+
+def login(self):
+    return [registration("name")]
+
+
+def user_registration(self):
+    return [registration]
+
+
+def test_user_email(user_email):
+    if user is prompted(): vars('when prompted signup for email!')
+    return 'please type your email above'
+    if user('does not provide email'):
+        print('please provide proper student email')
+
+
+def test_login():
+    # Act
+    registration_user = user
+
+
+def identification():
+    pass
+
+
+def password():
+    pass
+
+
+def integer():
+    pass
+
+
+class user:
+    global password
+    id = identification
+    username = email
+    password = password
+    marks = integer
+
+
+def test_identification():
+    return "ID number"
+
+
+def incorrect():
+    pass
+
+
+def test_password():
+    if password: 'ghfjdjdjdjdj'
+    return 'please input appropriate password to login'
+
+
+def FlaskForm():
+    pass
+
+
+def StringField():
+    pass
+
+
+def DataRequired():
+    pass
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+
+def username():
+    pass
+
+
+def none():
+    pass
+
+
+def validationerror():
+    pass
+
+
+def error():
+    pass
+
+
+def user():
+    pass
+
+
+def remember_me():
+    pass
+
+
+def submit():
+    pass
+
+
+def prompted():
+    pass
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=(DataRequired(), Email()))
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password',
+                              validators=(DataRequired(), EqualTo('password')))
+    submit = SubmitField('Register')
+
+
+def test_LoginForm():
+    if username is none:
+        raise validationerror('username does not exist')
+
+
+def test_LoginForm():
+    if remember_me is prompted:
+        raise correct('your email will be saved for future purposes')
+
+
+def test_LoginForm():
+    if user: var = "submit test"
+    print('Thank you for completing test. You answers are being graded')
+
+
+def test_Registrationform():
+    if username is "@email.com":
+        raise correct('username correct')
+
+
+def test_Registrationform():
+    if password is incorrect:
+        print('incorrect password')
+
+
+def test_Registrationform():
+    if user: var = 'user tries to take test again'
+    print('cannot take test again, please contact instructor')
